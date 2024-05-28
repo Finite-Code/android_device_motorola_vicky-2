d5a6cc0a79d482797c956d7e0d14f87f646c03c2
ShivamX087 <shivamx976@gmail.com>
2024-05-28 11:02:54 +0530

    vicky: Define Kernel Flags
    * Device uses Boot Header v4.
    * Ship builds with Google's GKI kernel for now.
    * Boot permissive for now.
    * Also define mkbootimg args while we are at it.
    
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,7 @@
 #
 
 DEVICE_PATH := device/motorola/vicky
+KERNEL_PATH := device/motorola/vicky-kernel
 BOARD_VENDOR := motorola
 
 # Architecture
@@ -44,3 +45,29 @@ TARGET_OTA_ASSERT_DEVICE := vicky
 
 # Build
 BUILD_BROKEN_DUP_RULES := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_BOOT_HEADER_VERSION := 4
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_OFFSET := 0x40000000
+BOARD_KERNEL_PAGESIZE := 0x00001000
+BOARD_TAGS_OFFSET := 0x47c80000
+BOARD_RAMDISK_OFFSET := 0x66f00000
+BOARD_RAMDISK_USE_LZ4 := true
+
+BOARD_MKBOOTIMG_ARGS += \
+    --dtb_offset $(BOARD_TAGS_OFFSET) \
+    --header_version $(BOARD_BOOT_HEADER_VERSION) \
+    --kernel_offset $(BOARD_KERNEL_OFFSET) \
+    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
+    --tags_offset $(BOARD_TAGS_OFFSET)
+
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_PREBUILT_BOOTIMAGE := $(wildcard $(KERNEL_PATH)/boot-5.10-gz.img)
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+TARGET_NO_KERNEL := true
+
+BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
