b0f78bb1e8bfccf2b6c0e655af83b55cb35087e7
ShivamX087 <shivamx976@gmail.com>
2024-05-29 14:49:16 +0530

    vicky: Inherit vendor_ramdisk.mk
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- device.mk
+++ device.mk
@@ -15,6 +15,28 @@
 
 DEVICE_PATH := device/motorola/vicky
 
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
@@ -36,7 +58,8 @@ PRODUCT_SHIPPING_API_LEVEL := 32
 # Init
 PRODUCT_PACKAGES += \
     init.mt6789.rc \
-    fstab.mt6789
+    fstab.mt6789 \
+    fstab.mtmt6789.vendor_ramdisk
 
 # Lights
 PRODUCT_PACKAGES += \
